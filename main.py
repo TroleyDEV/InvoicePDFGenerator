dae66d9ee76506822f9a24e0684df6daf1be81d4
Troley <troleyhs@gmail.com>
2024-03-09 00:52:36 +0100

    Found better way to calculate total price with .sum()

--- main.py
+++ main.py
@@ -44,9 +44,6 @@ for filepath in filepaths:
     pdf.cell(w=columns_w[3], h=8, txt=columns[3].title(), border=1, align="C")
     pdf.cell(w=0, h=8, txt=columns[4].title(), border=1, ln=1, align="C")
 
-    # Reset total price from previous document
-    total = 0
-
     # Loop for creating data tables from Excel data
     for index, row in df.iterrows():
         pdf.set_font(family="Times", size=9, style="B")
@@ -58,22 +55,19 @@ for filepath in filepaths:
         pdf.cell(w=columns_w[3], h=8, txt=str(row["price_per_unit"]), border=1)
         pdf.cell(w=0, h=8, txt=str(row["total_price"]), border=1, ln=1)
 
-        # Calculate total price
-        total = total + row["total_price"]
-
     # Create row for total price info
     pdf.cell(w=columns_w[0], h=8, txt="", border=1)
     pdf.cell(w=columns_w[1], h=8, txt="", border=1)
     pdf.cell(w=columns_w[2], h=8, txt="", border=1)
     pdf.cell(w=columns_w[3], h=8, txt="", border=1)
-    pdf.cell(w=0, h=8, txt=str(total), border=1, ln=1)
+    pdf.cell(w=0, h=8, txt=str(df["total_price"].sum()), border=1, ln=1)
 
     pdf.ln(20)
 
     # Set information of total amount and Company name, logo
     pdf.set_font(family="Arial", size=16, style="B")
     pdf.set_text_color(0, 0, 0)
-    pdf.cell(w=0, h=8, txt=f"The total due amount is {total} Euros.", ln=1)
+    pdf.cell(w=0, h=8, txt=f"The total due amount is {df["total_price"].sum()} Euros.", ln=1)
 
     pdf.cell(w=32, h=8, txt="PythonHow")
     pdf.image(name="pythonhow.png", w=8, h=8)
